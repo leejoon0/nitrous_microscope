62e11d37f5754fe35f5052625c0bec39a8df5cc0
leejoon0 <leejoon0@gmail.com>
2015-11-03 23:25:57 +0000

    Post등록페이지 추가, 헤더 페이지 링크 추가

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,14 @@
+Template.postSubmit.events({
+  'submit form': function(e){
+    e.preventDefault();
+    
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+    
+    post._id = Posts.insert(post);
+    Route.go('postPage', post);
+    
+  }
+});
