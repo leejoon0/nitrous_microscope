796d14b8500703f4f0e5901bfd923509416e527f
leejoon0 <leejoon0@gmail.com>
2015-11-04 02:49:30 +0000

    오류보고실제적용

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -11,10 +11,10 @@ Template.postSubmit.events({
     //Router.go('postPage', post);
     Meteor.call('postInsert', post, function(error, result){
       if(error)
-        return alert(error.reason);
+        return throwError(error.reason);
       
       if (result.postExists)
-        alert('This link has already been posted');
+        throwError('This link has already been posted');
       
       Router.go('postPage', {_id: result._id});
     });
